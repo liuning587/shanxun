6667de8096a246cd1e3967aa1e39cefb2416199f
liuning <liuning@liuning-PC>
2013-08-24 10:52:48 +0800

    提交腾达版本，但是未调通。

--- routedial1/src/routerdial.cpp
+++ routedial1/src/routerdial.cpp
@@ -13,6 +13,7 @@
  Section: Includes
  ----------------------------------------------------------------------------*/
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
 #include "log.h"
@@ -46,7 +47,7 @@
  */
 #if 0
 /* 水星路由器 */
-static const char *prouterchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap=0&acc=%s&psw=%s&linktype=4&waittime2=0&Connect=%%C1%%AC+%%BD%%D3 HTTP/1.1\r\n"
+static const char *pTPlinkchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap=0&acc=%s&psw=%s&linktype=4&waittime2=0&Connect=%%C1%%AC+%%BD%%D3 HTTP/1.1\r\n"
 "Accept: image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/QVOD, application/QVOD, */*\r\n"
 "Referer: http://192.168.1.1/userRpm/PPPoECfgRpm.htm\r\n"
 "Accept-Language: zh-cn\r\n"
@@ -58,7 +59,7 @@ static const char *prouterchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap
 #endif
 #if 1
 /* TP-LINK路由器 */
-static const char *prouterchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap=0&acc=%s&psw=%s&linktype=4&waittime2=0&Connect=%%C1%%AC+%%BD%%D3 HTTP/1.1\r\n"
+static const char *pTPlinkchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap=0&acc=%s&psw=%s&linktype=4&waittime2=0&Connect=%%C1%%AC+%%BD%%D3 HTTP/1.1\r\n"
         "Accept: */*\r\n"
         "Accept-Language: zh-cn\r\n"
         "UA-CPU: x86\r\n"
@@ -69,12 +70,44 @@ static const char *prouterchar = "GET /userRpm/PPPoECfgRpm.htm?wantype=2&VnetPap
         "Authorization: Basic YWRtaW46YWRtaW4=\r\n\r\n";
 ;
 #endif
+/* 腾达路由器 */
+#if 1
+/**
+ * 一下字符串有三个参数
+ * 1. 路由器地址
+ * 2. 用户名长度+密码长度+148
+ * 3：time(NULL)
+ * 4: 用户名
+ * 5：密码
+ */
+static const char *pTendaChar = "POST /goform/AdvSetWan HTTP/1.1\r\n"
+        "Accept: */*\r\n"
+        "Accept-Language: zh-cn\r\n"
+        "UA-CPU: x86\r\n"
+        "User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022; .NET CLR 2.0.50727)\r\n"
+        "Content-Type: application/x-www-form-urlencoded\r\n"
+        "Accept-Encoding: gzip, deflate\r\n"
+        "Host: %s\r\n"
+        "Content-Length:%d\r\n"
+        "Connection: Keep-Alive\r\n"
+        "Cache-Control: no-cache\r\n"
+        "Cookie: admin:languange=cn\r\n\r\n"
+        "GO=wan_connectd.asp&rebootTag=&v12_time=%d.738&WANT2=3&WANT1=2&PUN=%s&PPW=%s&MTU=1492&SVC=&AC=&PIDL=60&PCM=2&hour1=0&minute1=0&hour2=0&minute2=0";
+#endif
 /*-----------------------------------------------------------------------------
  Section: Local Function Prototypes
  ----------------------------------------------------------------------------*/
 static char serverip[16] = {0};
 static unsigned short port = 0u;
 static char logflag = 0;
+/**
+ * 路由器类型:
+ * 0: 水星
+ * 1: TP-link   WR710
+ * 2: TP-link   WR800
+ * 3: Tenda
+ */
+static char gateway_type = 3;   /* 路由器类型 */
 
 /*-----------------------------------------------------------------------------
  Section: Global Function Prototypes
@@ -108,7 +141,6 @@ extern"C" __declspec(dllexport) int _stdcall _RouterDial(char *pusr, char *passw
 {
     unsigned int socket = 0;
     char sendbuf[2048] = {0};
-    char *pusrname = NULL;
     int sendlen = 0;
 
     if ((socket = connect_start()) == 0)
@@ -120,14 +152,36 @@ extern"C" __declspec(dllexport) int _stdcall _RouterDial(char *pusr, char *passw
     log_print("in %s() arg: usr[0x%08x]:%s passwd[0x%08x]%s\n",
             __FUNCTION__, pusr, pusr, passwd, passwd);
 #endif
-    /* 将用户名转换为urlcode */
-    pusrname = url_encode(pusr);
-    sprintf(sendbuf, prouterchar, pusrname, passwd, serverip);
-    free(pusrname); /* 用完后释放 */
-    sendlen = strlen(sendbuf) + 1;
-    log_print("send:\n%s\n", sendbuf);
+
     do
     {
+        if (gateway_type == 0 || gateway_type == 1)
+        {
+            /* 水星和tplink */
+            /* 将用户名转换为urlcode */
+            char *pusrname = url_encode(pusr);
+            sprintf(sendbuf, pTPlinkchar, pusrname, passwd, serverip);
+            free(pusrname); /* 用完后释放 */
+            sendlen = strlen(sendbuf) + 1;
+        }
+        else if (gateway_type == 2)
+        {
+            log_print("暂时不支持TP-link 2\n");
+            break;
+        }
+        else if (gateway_type == 3)
+        {
+            int ContentLen = 149 + strlen(pusr) + strlen(passwd);
+            sprintf(sendbuf, pTendaChar, serverip, ContentLen, time(NULL), pusr, passwd);
+            sendlen = strlen(sendbuf) + 1;
+        }
+        else
+        {
+            log_print("暂时不支持gateway_type[%d]\n", gateway_type);
+            break;
+        }
+
+        log_print("send:\n%s\n", sendbuf);
         if (sendlen != socket_send(socket, sendbuf, sendlen))
         {
             log_print("%s发送数据数据出错\n", __FUNCTION__);
